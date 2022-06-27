d9dc904f429e2b50d3020b76845bf799b1afcec1
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-06-27 18:50:50 -0400

    Deployment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'FiftyShadesofCode') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{ asset('/assets/Favicon.png') }}">
 </head>
 <body>
     <div id="app">
